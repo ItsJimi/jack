24d5fa20997a10a011366473aabcf07daa157789
ItsJimi <jimi.maiquez@gmail.com>
2017-07-17 23:54:06 +0200

    Tests with yaml config file

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net/http"
-	"strconv"
+	"io/ioutil"
+	"log"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
+type config struct {
+	Port int64  `yaml:"port"`
+	Path string `yaml:"path"`
+}
+
 func main() {
-	port := flag.Int("port", 8080, "Port")
-	path := flag.String("path", ".", "Directory")
+	c := config{}
 
-	flag.Parse()
+	yamlFile, err := ioutil.ReadFile(".serve.yml")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	log.Print(yamlFile)
+	// port := flag.Int("port", 8080, "Port")
+	// path := flag.String("path", ".", "Directory")
 
-	fmt.Printf("Start on %d\n", *port)
-	http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(*path)))
+	// flag.Parse()
+
+	// fmt.Printf("Start on %d\n", *port)
+	// http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(*path)))
 }
